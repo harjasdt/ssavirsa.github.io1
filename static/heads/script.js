07f93daded0bf65aa742300d2be21d5445529de5
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-10-12 01:34:13 +0530

    final

--- static/heads/script.js
+++ static/heads/script.js
@@ -1,3 +1,36 @@
+
+if ($(".new-swiper-container").hasClass("new-team-member-slider")) {
+    var swiper = new Swiper('.new-swiper-container', {
+        slidesPerView: 2,
+        allowTouchMove: true,
+        loop: false,
+        centeredSlides: false,
+        slideToClickedSlide: true,
+        effect: "coverflow",
+        grabCursor: true,
+        autoplay: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        coverflow: {
+            rotate: 0,
+            stretch: 100,
+            depth: 200,
+            modifier: 1,
+            slideShadows: false
+        },
+        breakpoints: {
+            // when window width is <= 768px
+            767: {
+                slidesPerView: 1,
+                centeredSlides: false,
+                effect: "slide",
+            }
+        }
+    });
+    }
+
 if ($(".swiper-container").hasClass("team-member-slider")) {
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
